8c224aff1a9655884f88091b3489f6acdd824f49
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2024-12-05 14:27:45 +0100

    solve day 5 part 2

--- src/main/java/moe/ranka/aoc2024/D05.java
+++ src/main/java/moe/ranka/aoc2024/D05.java
@@ -2,6 +2,7 @@ package moe.ranka.aoc2024;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -47,8 +48,38 @@ public class D05 extends Day {
 
     @Override
     public void part2() {
-        // TODO Auto-generated method stub
+        var file = this.readFile("05.txt").split("\n\n");
+        List<int[]> rules = List.of(file[0].split("\n"))
+                .stream()
+                .map(x -> Arrays.stream(x.split("\\|"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray())
+                .collect(Collectors.toList());
+        List<List<Integer>> updates = List.of(file[1].split("\n"))
+                .stream()
+                .map(x -> Arrays.stream(x.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+        List<Integer> res = new ArrayList<>();
 
+        for (List<Integer> update : updates) {
+            if (!checkRules(update, rules)) {
+                update.sort(
+                        new Comparator<Integer>() {
+                            public int compare(Integer a, Integer b) {
+                                for (int[] rule : rules) {
+                                    if (rule[0] == b && rule[1] == a) {
+                                        return 1;
+                                    }
+                                }
+                                return -1;
+                            }
+                        });
+                res.add(update.get((update.size() - 1) / 2));
+            }
+        }
+        System.out.println(res.stream().mapToInt(Integer::valueOf).sum());
     }
 
 }
