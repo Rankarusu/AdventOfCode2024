36995714a826409ea06b12140db813c5d08e1df1
Rankarusu <48137583+Rankarusu@users.noreply.github.com>
2024-12-05 13:52:39 +0100

    solve day 5 part 1

--- src/main/java/moe/ranka/aoc2024/App.java
+++ src/main/java/moe/ranka/aoc2024/App.java
@@ -2,6 +2,6 @@ package moe.ranka.aoc2024;
 
 public class App {
     public static void main(String[] args) {
-        new D04().run();
+        new D05().run();
     }
 }
---
+++ src/main/java/moe/ranka/aoc2024/D05.java
@@ -0,0 +1,54 @@
+package moe.ranka.aoc2024;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class D05 extends Day {
+
+    @Override
+    public void part1() {
+        var file = this.readFile("05.txt").split("\n\n");
+        List<int[]> rules = List.of(file[0].split("\n"))
+                .stream()
+                .map(x -> Arrays.stream(x.split("\\|"))
+                        .mapToInt(Integer::parseInt)
+                        .toArray())
+                .collect(Collectors.toList());
+        List<List<Integer>> updates = List.of(file[1].split("\n"))
+                .stream()
+                .map(x -> Arrays.stream(x.split(","))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList()))
+                .collect(Collectors.toList());
+        List<Integer> res = new ArrayList<>();
+
+        for (List<Integer> update : updates) {
+            if (checkRules(update, rules)) {
+                res.add(update.get((update.size() - 1) / 2));
+            }
+        }
+
+        System.out.println(res.stream().mapToInt(Integer::valueOf).sum());
+    }
+
+    private boolean checkRules(List<Integer> update, List<int[]> rules) {
+        for (int[] rule : rules) {
+            var a = update.indexOf(rule[0]);
+            var b = update.indexOf(rule[1]);
+
+            if (a != -1 && b != -1 && b < a) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void part2() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
